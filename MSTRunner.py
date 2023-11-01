99f47711cddea24963d6e59d9a20b202646ce551
harlanhowe <harlan.howe@kinkaid.org>
2023-10-31 22:55:05 -0500

    Style fixes on previously commented code

--- MSTRunner.py
+++ MSTRunner.py
@@ -7,9 +7,9 @@ from UndirectedGraphFile import UndirectedGraph
 def main():
     graph = UndirectedGraph(filename="UndirectedGraph1.txt")
     window = graph.draw_self(caption="Original")
-    # mst = MST(graph,method = MST.METHOD_KRUSKAL)
-    #
-    # window = mst.draw_self(origin= (0,400),window=window,caption="Kruskal",color=(1.0,0.75,0.25))
+    mst = MST(graph, method=MST.METHOD_KRUSKAL)
+
+    window = mst.draw_self(origin=(0, 400), window=window, caption="Kruskal", color=(1.0, 0.75, 0.25))
 
     cv2.imshow("MST", window)
     cv2.waitKey()
